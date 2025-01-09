649b6431ef7f12f74c16389716e2dac7aafe98d5
Maira Dawood <mdawood@c2r3s9.42london.com>
2025-01-09 18:05:05 +0000

    mod

--- printf.c
+++ printf.c
@@ -11,10 +11,39 @@
 /* ************************************************************************** */
 #include "libftprintf.h"
 #include <stdio.h>
+#include <string.h>
+
+char   *str_print(char const *s, void (*f)(char, int), va_list list)
+{
+    int i;
+    char  *ptr;
+    int result;
+
+    if (s == NULL)
+        return (NULL);
+    ptr = malloc(strlen(s) + 1);
+    if (ptr == NULL)
+        return (NULL);
+    i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == '%' && s[i+1] == 'c')
+            f(result = va_arg(list, int), 1);
+        i++;
+    }
+    ptr[i] = '\0';
+    return (ptr);
+}
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
+}
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, strlen(s));
 }
 
 int	ft_printf(const char *data, ...)
@@ -22,15 +51,18 @@ int	ft_printf(const char *data, ...)
 	va_list list;
 	va_start(list, data);
 	int i = 0;
-	int count = 0;
+    int count = 0;
+
 	while (data[i] != '\0')
 	{
-		if (data[i] == '%' && data[i+1] == 'c')
-		{
-			int result = va_arg(list, int);
-			ft_putchar_fd(result, 1); 
-			count++;
-		}
+		if (data[i] == '%')
+        {
+            if (data[i+1]  == 'c')
+                ft_putchar_fd(va_arg(list, int), 1);
+            if (data[i+1] == 's')
+                ft_putstr_fd(va_arg(list, char *), 1);
+
+        }
 		i++;
 	}
 	va_end(list);
@@ -41,6 +73,10 @@ int main(void)
 	int ans;
 	char c = 'a';
 	ans = ft_printf("%c", c);
-	printf("\n%d", ans);
+    //printf("\n%d", ans);
+    char *s = "hello";
+    ans = ft_printf("%s", s);
+    printf("Size of long: %zu bytes\n", sizeof(long));
+	//printf("\n%d", ans);
 	//when i run this it is supposed to print a. 
 }
--- test.c
+++ test.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
-char   *str_print(char const *s, char (*f)(int, char), va_list list)
+char   *str_print(char const *s, void (*f)(char, int), va_list list)
 {
     int i;
-    char   *ptr;
+    char  *ptr;
+    int result;
 
     if (s == NULL)
         return (NULL);
@@ -16,10 +17,7 @@ char   *str_print(char const *s, char (*f)(int, char), va_list list)
     while (s[i] != '\0')
     {
         if (s[i] == '%' && s[i+1] == 'c')
-        {
-            int result = va_arg(list, int);
-            ptr[i] = f(result, result);
-        }
+            f(result = va_arg(list, int), 1);
         i++;
     }
     ptr[i] = '\0';
@@ -30,29 +28,49 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-
-
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, strlen(s));
+}
+void    ft_putadd_fd(char *s, int fd)
+{
+    
+}
 int	ft_printf(const char *data, ...)
 {
 	va_list list;
 	va_start(list, data);
 	int i = 0;
-	int count = 0;
+    int count = 0;
 
+	while (data[i] != '\0')
+	{
+		if (data[i] == '%')
+        {
+            if (data[i+1]  == 'c')
+                ft_putchar_fd(va_arg(list, int), 1);
+            if (data[i+1] == 's')
+                ft_putstr_fd(va_arg(list, char *), 1);
+            if (data[i+1] == 'p')
+                ft_putadd_fd(va_arg(list, char *), 1);
 
-    char *result = str_print(data, ft_putchar_fd, list);
-    printf("%s", result);
-	// while (data[i] != '\0')
-	// {
-	// 	ft_strmapi(data, ft_putchar_fd);
-	// 	// if (data[i] == '%' && data[i+1] == 'c')
-	// 	// {
-	// 	// 	int result = va_arg(list, int);
-	// 	// 	ft_putchar_fd(result, 1); 
-	// 	// 	count++;
-	// 	// }
-	// 	//i++;
-	// }
+        }
+		i++;
+	}
 	va_end(list);
 	return (count);
+}
+int main(void)
+{
+	int ans;
+	char c = 'a';
+	ans = ft_printf("%c", c);
+    //printf("\n%d", ans);
+    char *s = "hello";
+    ans = ft_printf("%s", s);
+    printf("Size of long: %zu bytes\n", sizeof(long));
+	//printf("\n%d", ans);
+	//when i run this it is supposed to print a. 
 }
