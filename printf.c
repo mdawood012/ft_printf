c3cb7e6872cde006087ba735802537a2b226f112
Maira Dawood <mdawood@c2r2s9.42london.com>
2025-01-18 16:39:21 +0000

    mod

--- printf.c
+++ printf.c
@@ -13,9 +13,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
+    return (1);
 }
 
 int	ft_toupper(int c)
@@ -27,18 +28,19 @@ int	ft_toupper(int c)
 	return (c);
 }
 
-void   long_to_hex(long num, int capital)
+int   long_to_hex(long num, int capital)
 {
     char *hex_values;
-    char *values;
+    char values[20];
     int i;
+    int count; 
     int remainder;
 
     hex_values = "0123456789abcdef";
-	values = malloc(20);
+    count = 0;
 	i = 0;
-	if (!values || num == 0)
-        return ;
+	if (num == 0)
+        return 0;
     while (num > 0)
     {
         remainder = num % 16;
@@ -48,61 +50,76 @@ void   long_to_hex(long num, int capital)
 	while (i > 0)
     {
         if (capital)
-		    ft_putchar_fd(ft_toupper(values[--i]), 1);
+		    count += ft_putchar_fd(ft_toupper(values[--i]), 1);
         else
-            ft_putchar_fd(values[--i], 1);
+            count += ft_putchar_fd(values[--i], 1);   
     }
-	free(values);
+    return count;
 }
 
-void	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
+    int count;
 	if (!s)
-		return ;
-	write(fd, s, strlen(s));
+		return (0);
+    count = strlen(s);
+	write(fd, s, count);
+    return (count);
 }
 
-void    ft_putptr_fd(char *s, int fd, int capital)
+int    ft_putptr_fd(char *s, int fd, int capital)
 {
-    long long address = (long long)s;
+    int count;
+    long address = (long)s;
     ft_putchar_fd('0', fd);
     ft_putchar_fd('x', fd);
-    long_to_hex(address, capital);
+    count = long_to_hex(address, capital) + 2;
+    return count;
 }
 
-void	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(int n, int fd)
 {
+    int     count;
 	long	num;
 
+    count = 0;
 	num = (int) n;
 	if (num < 0)
 	{
-		ft_putchar_fd('-', fd);
+		count += ft_putchar_fd('-', fd);
 		num = -1 * num;
 	}
 	if ((num / 10) > 0)
 	{
-		ft_putnbr_fd(num / 10, fd);
+		count += ft_putnbr_fd(num / 10, fd);
 		ft_putchar_fd(num % 10 + '0', fd);
 	}
 	else
 		ft_putchar_fd(num % 10 + '0', fd);
+    count++;
+    return count;
 }
 
-void ft_putuinbr_fd(unsigned int num, int fd)
+int ft_putuinbr_fd(unsigned int num, int fd)
 {
+    int count;
+    count = 0;
     if ((num / 10) > 0)
 	{
 		ft_putuinbr_fd(num / 10, fd);
-		ft_putchar_fd(num % 10 + '0', fd);
+		count += ft_putchar_fd(num % 10 + '0', fd);
 	}
 	else
-		ft_putchar_fd(num % 10 + '0', fd);
+		count += ft_putchar_fd(num % 10 + '0', fd);
+    return count;
 }
 
-void  ft_nbrthex_fd(int num, int capital)
+int  ft_nbrthex_fd(int num, int capital)
 {
-    long_to_hex(num, capital);
+    int count;
+    count = long_to_hex(num, capital);
+    return count;
+
 }
 
 int	ft_printf(const char *data, ...)
@@ -119,22 +136,22 @@ int	ft_printf(const char *data, ...)
         {
             if (data[i+1]  == 'c')
             {
-                ft_putchar_fd(va_arg(list, int), 1);
+                count += ft_putchar_fd(va_arg(list, int), 1);
                 i++;
             }
             if (data[i+1] == 's')
             {    
-                ft_putstr_fd(va_arg(list, char *), 1);
+                count += ft_putstr_fd(va_arg(list, char *), 1);
                 i++;
             }
             if (data[i+1] == 'p')
             {
-                ft_putptr_fd(va_arg(list, char *), 1, capital);
+                count += ft_putptr_fd(va_arg(list, char *), 1, capital > data[i+1]);
                 i++;
             }
             if (data[i+1] == 'd' || data[i+1] == 'i')
             {    
-                ft_putnbr_fd(va_arg(list, int), 1);
+                count += ft_putnbr_fd(va_arg(list, int), 1);
                 i++;
             }
             if (data[i+1] == 'u')
@@ -154,27 +171,19 @@ int	ft_printf(const char *data, ...)
             }
         }
         else
+        {
             ft_putchar_fd(data[i], 1);
+            count++;
+        }
 		i++;
 	}
 	va_end(list);
 	return (count);
 }
 
-int main(void)
-{
-    int a = 0;
-    int *ptr =  &a;
-    ft_printf("m/y ans: %p\n", ptr);
-    int dec = 012;
-    ft_printf("in %%i: %i\n", dec);
-    dec = 012;
-    ft_printf("in dec: %d\n", dec);
-    int num = -10;
-    ft_printf("in unsigned int: %u\n", (unsigned int)num);
-    //printf("Size of long long: %I64d bytes\n", sizeof(long long));
-    int n = 56088;
-    ft_printf("in hex form: %x\n", n);
-    ft_printf("in capitalised hex form: %X", n);
-    //ft_printf("%x\n", n);
-}
+// int main(void)
+// {
+//     int num = 2;
+//     int result = ft_printf("%d", num);
+//     ft_printf("%d", result);
+// }
