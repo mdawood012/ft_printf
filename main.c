9e8ec88038216bd0642e01468d1d03dc2b130272
Maira Dawood <mdawood@c2r2s6.42london.com>
2025-01-20 17:26:12 +0000

    mod

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
-#include "ft_printf.h"
+#include <stdio.h>
+#include "libftprintf.h"
+#include <stddef.h> 
+#include "limits.h"
 
-int main()
+int main(void)
 {
-     int num = 2;
-     int result = ft_printf("%d", num);
-     ft_printf("%d", result);
+    ft_printf(" %p %p ", 0, 0);
+    return (0);
 }
+// //now i've got to figure out the p category. i think it might be problematic when it
+// //comes to pointers where it is equal to null. 
+// //instead it has issues when the memory adddress is LONG_MIN, LONG_MAX etc. 
+//test 6, 8, 9. 
--- putstr_fd.c
+++ putstr_fd.c
@@ -3,9 +3,17 @@
 int	my_putstr_fd(char *s, int fd)
 {
     int count;
-	if (!s)
-		return (0);
+    char *temp = "(null)";
+	count = 0;
+    if (s == NULL)
+    {
+        count += 6;
+        write(1, temp, 6);
+        return (count);
+    }
     count = ft_strlen(s);
 	write(fd, s, count);
     return (count);
 }
+
+//ive got to count every single character and so in this case it should be 18. 
